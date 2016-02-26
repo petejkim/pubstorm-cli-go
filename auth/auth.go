eab1773450161742e69dc1022bec9b956efcd416
Peter Jihoon Kim <raingrove@gmail.com>
2016-02-26 02:01:20 +0000

    Accept confirmation code after sign up

--- auth/auth.go
+++ auth/auth.go
@@ -2,8 +2,9 @@ package auth
 
 import (
 	"fmt"
-	"log"
+	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/codegangsta/cli"
 	"github.com/franela/goreq"
@@ -45,12 +46,16 @@ func Signup(c *cli.Context) {
 	}.Do()
 
 	if err != nil {
-		log.Fatal(err)
+		util.CouldNotMakeRequest(err)
+	}
+
+	if res.StatusCode != http.StatusCreated && res.StatusCode != 422 {
+		util.HandleErrorResponse(res)
 	}
 
 	var j map[string]interface{}
 	if err := res.Body.FromJsonTo(&j); err != nil {
-		log.Fatal(err)
+		util.SomethingWentWrong(err)
 	}
 
 	if res.StatusCode == 422 && j["error"] == "invalid_params" {
@@ -58,6 +63,45 @@ func Signup(c *cli.Context) {
 		if errs, ok := j["errors"].(map[string]interface{}); ok {
 			for k, v := range errs {
 				fmt.Println("*", k, v)
+			}
+		}
+	} else if res.StatusCode == http.StatusCreated {
+		fmt.Println("Your account has been created. You will receive your confirmation code shortly.")
+
+		for {
+			confirmationCode, err := readline.Read("Enter Confirmation Code (Check your inbox!): ")
+			util.ExitIfError(err)
+
+			res, err := goreq.Request{
+				Method:      "POST",
+				Uri:         config.Host + "/user/confirm",
+				ContentType: "application/x-www-form-urlencoded",
+
+				Body: url.Values{
+					"email":             {email},
+					"confirmation_code": {confirmationCode},
+				}.Encode(),
+			}.Do()
+
+			if err != nil {
+				util.CouldNotMakeRequest(err)
+			}
+
+			if res.StatusCode != http.StatusOK && res.StatusCode != 422 {
+				util.HandleErrorResponse(res)
+			}
+
+			if res.StatusCode == 422 {
+				resText, err := res.Body.ToString()
+				if err != nil {
+					util.SomethingWentWrong(err)
+				}
+				if strings.Contains(resText, "invalid email or confirmation_code") {
+					fmt.Println("You've entered an incorrect confirmation code. Please try again.")
+				}
+			} else if res.StatusCode == http.StatusOK {
+				fmt.Println("Thanks for confirming your email address! Your account is now active!")
+				return
 			}
 		}
 	}
--- util/util.go
+++ util/util.go
@@ -1,9 +1,12 @@
 package util
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"os"
+
+	"github.com/franela/goreq"
 )
 
 func ExitIfError(err error) {
@@ -13,4 +16,32 @@ func ExitIfError(err error) {
 		}
 		os.Exit(1)
 	}
+}
+
+func HandleErrorResponse(res *goreq.Response) {
+	sc := res.StatusCode
+	var j map[string]interface{}
+	if err := res.Body.FromJsonTo(&j); err == nil {
+		e := j["error"]
+		ed := j["error_description"]
+
+		if e != nil {
+			if ed != nil {
+				log.Fatal(fmt.Sprintf("%d: %s - %s", sc, e, ed))
+			}
+			log.Fatal(fmt.Sprintf("%d: %s", sc, e))
+		}
+	} else if str, err := res.Body.ToString(); err == nil {
+		log.Fatal(fmt.Sprintf("%d: %s", sc, str))
+	}
+
+	log.Fatal(fmt.Sprintf("%d: Something went wrong. Please try again.", sc))
+}
+
+func SomethingWentWrong(err error) {
+	log.Fatal("Something went wrong. Please try again.\n", err)
+}
+
+func CouldNotMakeRequest(err error) {
+	log.Fatal("Failed to make request to Rise server. Please check your Internet connection and try again.", err)
 }
