e0bf0b8fb31f6314e11f4752d846e7248a4c1ff4
Peter Jihoon Kim <raingrove@gmail.com>
2016-03-03 00:32:16 +0000

    converted pathmatch test to use ginkgo

--- pkg/pathmatch/pathmatch_test.go
+++ pkg/pathmatch/pathmatch_test.go
@@ -1,58 +1,63 @@
-package pathmatch
+package pathmatch_test
 
-import "testing"
+import (
+	"testing"
 
-func TestPathMatch(t *testing.T) {
-	testPathMatch := func(path, pattern string, match bool) {
-		if v := PathMatch(path, pattern); v != match {
-			if match {
-				t.Errorf("expected PathMatch to find %s in %s", pattern, path)
-			} else {
-				t.Errorf("expected PathMatch not to find %s in %s", pattern, path)
-			}
-		}
-	}
-	testPathMatch("/foo/bar/123/.unison/foo/bar", ".unison", true)
-	testPathMatch("/foo/bar/123/.unison", ".unison", true)
-	testPathMatch(".unison/foo/bar/baz", ".unison", true)
+	"github.com/nitrous-io/rise-cli-go/pkg/pathmatch"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+)
 
-	testPathMatch("/foo/bar/123/.unisonn/foo/bar", ".unison", false)
-	testPathMatch("/foo/bar/123/.unisonn", ".unison", false)
-	testPathMatch(".unisonn/foo/bar/baz", ".unison", false)
-
-	testPathMatch("/foo/bar/123/..unison/foo/bar", ".unison", false)
-	testPathMatch("/foo/bar/123/..unison", ".unison", false)
-	testPathMatch("..unison/foo/bar/baz", ".unison", false)
-
-	testPathMatch("/foo/bar/123/tmp/sessions/foo/bar", "tmp/sessions", true)
-	testPathMatch("/foo/bar/123/tmp/sessions", "tmp/sessions", true)
-	testPathMatch("tmp/sessions/foo/bar/baz", "tmp/sessions", true)
-
-	testPathMatch("/foo/bar/123/tmp/sessionss/foo/bar", "tmp/sessions", false)
-	testPathMatch("/foo/bar/123/tmp/sessionss", "tmp/sessions", false)
-	testPathMatch("tmp/sessionss/foo/bar/baz", "tmp/sessions", false)
-
-	testPathMatch("/foo/bar/123/ttmp/sessions/foo/bar", "tmp/sessions", false)
-	testPathMatch("/foo/bar/123/ttmp/sessions", "tmp/sessions", false)
-	testPathMatch("ttmp/sessions/foo/bar/baz", "tmp/sessions", false)
+func Test(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "pathmatch")
 }
 
-func TestPathMatchAny(t *testing.T) {
-	testPathMatchAny := func(path string, patterns []string, match bool) {
-		if v := PathMatchAny(path, patterns...); v != match {
-			if match {
-				t.Errorf("expected PathMatchAny to find %+v in %s", patterns, path)
-			} else {
-				t.Errorf("expected PathMatchAny not to find %+v in %s", patterns, path)
-			}
-		}
-	}
+var _ = Describe("Pathmatch", func() {
+	DescribeTable("PathMatch",
+		func(path, pattern string, match bool) {
+			v := pathmatch.PathMatch(path, pattern)
+			Expect(v).To(Equal(match))
+		},
 
-	testPathMatchAny("/foo/bar/123/.unison/foo/bar", []string{".unison"}, true)
-	testPathMatchAny("/foo/bar/123/.unison", []string{".unison", "foo/bar"}, true)
-	testPathMatchAny(".unison/foo/bar/baz", []string{".unison", "bar"}, true)
-	testPathMatchAny(".unison/foo/bar/baz", []string{".unison", "bar/baz"}, true)
-	testPathMatchAny(".unison/foo/bar/baz", []string{".funison", "bar/baz"}, true)
+		Entry("matches", "/foo/bar/123/.unison/foo/bar", ".unison", true),
+		Entry("matches", "/foo/bar/123/.unison", ".unison", true),
+		Entry("matches", ".unison/foo/bar/baz", ".unison", true),
 
-	testPathMatchAny(".unison/foo/bar/baz", []string{".funison", "bar/baw"}, false)
-}
+		Entry("matches", "/foo/bar/123/tmp/sessions/foo/bar", "tmp/sessions", true),
+		Entry("matches", "/foo/bar/123/tmp/sessions", "tmp/sessions", true),
+		Entry("matches", "tmp/sessions/foo/bar/baz", "tmp/sessions", true),
+
+		Entry("does not match", "/foo/bar/123/.unisonn/foo/bar", ".unison", false),
+		Entry("does not match", "/foo/bar/123/.unisonn", ".unison", false),
+		Entry("does not match", ".unisonn/foo/bar/baz", ".unison", false),
+
+		Entry("does not match", "/foo/bar/123/..unison/foo/bar", ".unison", false),
+		Entry("does not match", "/foo/bar/123/..unison", ".unison", false),
+		Entry("does not match", "..unison/foo/bar/baz", ".unison", false),
+
+		Entry("does not match", "/foo/bar/123/tmp/sessionss/foo/bar", "tmp/sessions", false),
+		Entry("does not match", "/foo/bar/123/tmp/sessionss", "tmp/sessions", false),
+		Entry("does not match", "tmp/sessionss/foo/bar/baz", "tmp/sessions", false),
+
+		Entry("does not match", "/foo/bar/123/ttmp/sessions/foo/bar", "tmp/sessions", false),
+		Entry("does not match", "/foo/bar/123/ttmp/sessions", "tmp/sessions", false),
+		Entry("does not match", "ttmp/sessions/foo/bar/baz", "tmp/sessions", false),
+	)
+
+	DescribeTable("PathMatchAny",
+		func(path string, patterns []string, match bool) {
+			v := pathmatch.PathMatchAny(path, patterns...)
+			Expect(v).To(Equal(match))
+		},
+
+		Entry("matches", "/foo/bar/123/.unison/foo/bar", []string{".unison"}, true),
+		Entry("matches", "/foo/bar/123/.unison", []string{".unison", "foo/bar"}, true),
+		Entry("matches", ".unison/foo/bar/baz", []string{".unison", "bar"}, true),
+		Entry("matches", ".unison/foo/bar/baz", []string{".unison", "bar/baz"}, true),
+		Entry("matches", ".unison/foo/bar/baz", []string{".funison", "bar/baz"}, true),
+
+		Entry("does not match", ".unison/foo/bar/baz", []string{".funison", "bar/baw"}, false),
+	)
+})
